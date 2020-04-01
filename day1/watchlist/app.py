d036848878c6957c057f7928af073e34266f75c6
Bruce <1046244623@qq.com>
2020-04-01 17:48:27 +0800

    静态资源

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -10,7 +10,13 @@ def index():
         {"title":"战狼","year":"2020"},
         {"title":"心花路放","year":"2018"},
         {"title":"速度与激情8","year":"2012"},
-        {"title":"我的父亲母亲","year":"1995"}
+        {"title":"我的父亲母亲","year":"1995"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"}
     ]
     return render_template('index.html',name=name,movies=movies)
 
