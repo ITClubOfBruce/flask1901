973c14756533862cdc45e72b406c2a01d07a7955
Bruce <1046244623@qq.com>
2020-04-01 16:37:52 +0800

    第一个Flask项目和环境变量配置

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -3,5 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello，Flask</h1>"
+    return "<h1>Hello，Flask 中国</h1>"
 
+# 动态URL
+@app.route('/index/<name>')
+def home(name):
+    return "<h1>Hello，%s</h1>"%name
